74209045564c16c6789842b752f76c73f203a1a9
EC2 Default User <ec2-user@ip-172-31-22-93.ap-northeast-1.compute.internal>
2023-07-15 13:35:43 +0000

    マイページ及び退会機能

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,40 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_public!
-  
-  def show
+
+  def show #顧客のマイページ
+    @customer = current_customer
   end
-  
-  def edit
+
+  def edit #顧客の登録情報編集画面
+    @customer = current_customer
   end
-  
+
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:success] = "登録情報を更新しました。"
+      redirect_to customers_show_path
+    else
+      flash.now[:danger] = "登録に失敗しました。"
+      render 'edit'
+    end
   end
-  
-  def quit
+
+  def quit #顧客の退会確認画面
+
   end
-  
+
   def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: false) #ステータスの更新
+    reset_session #ログアウトさせる
+    redirect_to root_path
   end
+
+
+  private
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+      #requireメソッドがデータのオブジェクト名を定め、permitメソッドで変更を加えられる（保存の処理ができる）キーを指定
+    end
 end
