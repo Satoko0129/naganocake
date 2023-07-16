830a770b29f7213c6e5fe664f4b1f8b22f5c8f1c
EC2 Default User <ec2-user@ip-172-31-22-93.ap-northeast-1.compute.internal>
2023-07-16 12:24:22 +0000

    商品新規登録画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,18 @@ scope module: 'public' do
 end
 
 namespace :public do
-  resources :genres, only: [:show]
-  patch 'customers/withdraw' => 'customers#withdraw', as: 'customers_withdraw'
+  resources :genres, only: [:index]
+  patch 'customers/withdraw' => 'customers#withdraw', as: 'customers_withdraw' #asオプション=ルーティングに名前を指定出来る
   get 'show' => 'customers#show'
   get 'customers/edit' => 'customers#edit'
+  patch 'update' => 'customers#update'
+  get 'quit' => 'customers#quit'
+  post 'log' => 'orders#log', as: 'orders_log'
+  get 'thanks' => 'orders#thanks'
+  resources :orders, only: [:new, :create, :index, :show] #[]内のアクションのみを指定
+  resources :cart_items, only: [:index, :create, :update, :destroy]
+  delete 'cart_items' => 'cart_items#destroy_all', as: 'destroy_all'
+  resources :addresses, only: [:index, :edit, :create, :update, :destroy]
 end
 
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
@@ -24,6 +32,11 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
 namespace :admin do
   root to: 'homes#top'
+  resources :customers, only: [:index, :show, :edit, :update]
+  resources :genres, only: [:index, :edit, :create, :update]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :orders, only: [:show, :update]
+  resources :order_details, only: [:update]
 end
 
 
