0edd38983df34bfedbdb30c3ad22c32e02c3f109
EC2 Default User <ec2-user@ip-172-31-22-93.ap-northeast-1.compute.internal>
2023-07-29 10:08:36 +0000

    カート内商品の個変更

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,7 +1,14 @@
 class Public::CartItemsController < ApplicationController
-  before_action :authenticate_public!
+  before_action :authenticate_admin!
 
   def index
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+    #カートに入っている商品の合計金額。カート内商品の一覧画面で、カート内商品の合計金額を算出。
+    #inject・・・eachやmapのように繰り返しを行うメソッド。
+    #inject(0)・・・配列の合計を算出。　配列オブジェクト.inject { |初期値, 要素| ブロック処理 }　が基本の形
+    #1回目の動作・・・@cart_itemsから、カートに入っている1つ目のitemを取り出して、sumにitem.sum_of_priceを足す。(sumの初期値はinject(0)で0)
+    #2回目以降のづ動作・・・次のitemを取り出して、sumにitem.sum_of_priceを足す。これで最終的にカート内商品の合計金額が出る。
   end
 
   def update
@@ -14,5 +21,25 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = CartItem.new(params_cart_item) #Cart_itemの新規投稿。ストロングパラメーターで、個数、item_id、customer_idを受け取っている。
+    @cart_item.customer_id = current_customer.id
+    @cart_items = current_customer.cart_items.all #ここで、カートに入っているものを取り出している。
+
+      @cart_items.each do |cart_item| #カートの中身を一個ずつ取り出して、
+        if cart_item.item_id == @cart_item.item_id #もし、同じ商品が存在していれば、
+        new_amount = cart_item.amount + @cart_item.amount #今の個数に足す。それを、new_amountに代入
+        cart_item.update_attribute(:amount, new_amount) #cart_itemモデルのamountをnew_acountに更新するよ
+        @cart_item.delete #同一商品に関しては、個数だけ更新出来たら後は消去
+        end
+      end
+
+      @cart_item.save
+      redirect_to cart_items_path, notice: "カートに商品が入りました"
   end
+
+
+  private
+    def params_cart_item
+      params.require(:cart_item).permit(:amount, :item_id, :customer_id)
+    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
-  has_many :orders, through: :order_details #order_detailsを経由してordersのオブジェクトを取得する
-  has_many :order_details #中間テーブル　※throughオプションを使うときに必要
-  belongs_to :genre
-  attachment :image_id
+    has_many :orders, through: :order_details #order_detailsを経由してordersのオブジェクトを取得する
+    has_many :order_details #中間テーブル　※throughオプションを使うときに必要
+    belongs_to :genre
+    attachment :image_id
+
+  def taxin_price
+    price*1.1
+  end
+
 end
